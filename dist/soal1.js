1f92c9ccd9f9098e2b3c15331ec39432cd30dcf8
Sagi Setiono <2382016@unai.edu>
2025-02-05 22:36:57 +0700

    Update soal1.js

---
+++ dist/soal1.js
@@ -0,0 +1,21 @@
+"use strict";
+function countingValleys(steps, path) {
+    let level = 0;
+    let valleys = 0;
+    for (let i = 0; i < steps; i++) {
+        if (path[i] === 'U') {
+            level++;
+            if (level === 0) {
+                valleys++;
+            }
+        }
+        else {
+            level--;
+        }
+    }
+    return valleys;
+}
+// Contoh penggunaan
+const steps = 8;
+const path = "UDDDUDUU";
+console.log(countingValleys(steps, path));
